f68c1876f6a2f1b341af1bfdcf97f4129e40a8a3
Borja Roux <borja.roux@gmail.com>
2018-06-26 20:28:07 +0200

    Improved output

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ cp $TMP/$FILE.md $TMP/LATEST-$INTERFIX.md
 #pandoc $TMP/$FILE.md -f markdown $TOC -t html -s -o $TMP/$FILE.html
 pandoc $TMP/$FILE.md -f markdown $TOC -t html -s -o $TMP/LATEST-$INTERFIX.html
 pandoc $TMP/$FILE.md -f markdown $TOC -s -o $TMP/LATEST-$INTERFIX.docx
-pandoc $TMP/$FILE.md -f markdown $TOC -s -o $TMP/LATEST-$INTERFIX.pdf
+pandoc -N --template=template.tex --variable toc-depth=4 --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=$NOW tmp/LATEST-id_vision.md --pdf-engine=xelatex --toc -o $TMP/LATEST-$INTERFIX.pdf
---
+++ src/00-Header.md
@@ -0,0 +1,3 @@
+% A personal vision in digital identities in banking
+% Borja Roux Lorenzo
+% June 2018
